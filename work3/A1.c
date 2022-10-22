cad03644d72f256ef5613d0d90e16b5a4e8a7854
qgr <114471921+qgr007@users.noreply.github.com>
2022-10-22 21:08:09 +0800

    Delete work3 directory

--- work3/A1.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-void print1(int arg)
-{
-	printf("A2 printf arg:%d\n",arg);
-}
--- work3/A2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-void print2(char *arg)
-{
-	printf("A2 printf arg:%s\n",arg);
-}
-
