fb04d71345dc8079923f4814eccf6e07d5deec07
qgr <114471921+qgr007@users.noreply.github.com>
2022-10-22 21:08:23 +0800

    Delete work4 directory

--- work4/sub1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "sub1.h"
-float x2x(int a,int b)
-{
-	float x;
-	x=(float)(a+b)/2;
-	return x;
-}
--- work4/sub1.h
+++
@@ -1,5 +0,0 @@
-#ifndef SUB1_H
-#define SUB1_H
-float x2x(int a,int b);
-float x2y(int a,int b);
-#endif
